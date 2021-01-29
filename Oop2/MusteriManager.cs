6bc764fe083d7390b0b855207d6c9a39934f6186
ErenBeykent <erengozsuz28@gmail.com>
2021-01-29 23:39:05 +0300

    23 ocak ders sonu

---
+++ Oop2/MusteriManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    class MusteriManager
+    {
+        //iş sınıflaro
+        public void Ekle(Musteri musteri)
+        {
+
+        }
+    }
+}
